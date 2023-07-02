9e94bf1109cc71324f53fb7d466df25fc697088f
programador-do-cerrado <programadordocerrado@gmail.com>
2023-07-02 20:28:32 -0300

    Criação do exercicio 44  - exercicio completo

--- Exercicios/ex045.py
+++ Exercicios/ex045.py
@@ -1,5 +1,14 @@
 """
 Crie um programa que faça o computador jogar Jokenpô com você.
-
-
 """
+import random
+
+print(
+    """
+    Suas opções: 
+    [ 0 ] PEDRA
+    [ 1 ] PAPEL
+    [ 2 ] TESOURA
+    """
+)
+usuario = int(input("Digite sua jogada "))
