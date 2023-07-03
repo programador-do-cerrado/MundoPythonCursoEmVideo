86a546d11a9342c2f85e78a0ea113ec10ff93d15
programador-do-cerrado <programadordocerrado@gmail.com>
2023-07-03 20:49:54 -0300

    Exercico aula 43

--- Exercicios/ex046.py
+++ Exercicios/ex046.py
@@ -2,3 +2,9 @@
 Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifício,
 indo de 10 até 0, com uma pausa de 1 segundo entre eles.
 """
+from time import sleep
+
+for f in range(10, 0, -1):
+    print("Contagem regressiva: {} ".format(f))
+    sleep(0.3)
+print("Feliz ano novo")
