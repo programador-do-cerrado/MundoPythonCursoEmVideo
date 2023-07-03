d19fce9aae61b44011188c34cd52580b881ecd74
programador-do-cerrado <programadordocerrado@gmail.com>
2023-07-03 19:54:40 -0300

    Criação do exercicio 44  - exercicio completo

--- Aulas/aula013.py
+++ Aulas/aula013.py
@@ -15,3 +15,8 @@ for c in range(1, 6):
     print("Oi! dentro do laço ")
     print("Fim dentro do laço - cuidado com a identação.")
 print("Fim. Fora do laço")
+
+for d in range(1, 7):
+    print(d)
+    "Conta de 1 até 6 pois ele ignora o último valor"
+print("Fim!")
