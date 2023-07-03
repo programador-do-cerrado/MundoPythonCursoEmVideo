6de95708c06974076860bfa8076668fef5907c65
programador-do-cerrado <programadordocerrado@gmail.com>
2023-07-03 20:23:35 -0300

    Criação do exercicio 44  - exercicio completo

--- Aulas/aula013.py
+++ Aulas/aula013.py
@@ -2,6 +2,7 @@
     Exemplos de código em python
     para criar laços de repetições
 """
+
 print("Oi")
 print("Oi")
 print("Oi")
@@ -20,3 +21,22 @@ for d in range(1, 7):
     print(d)
     "Conta de 1 até 6 pois ele ignora o último valor"
 print("Fim!")
+
+for f in range(30, 0, -1):
+    print("Contagem regressiva {}".format(f))
+
+g = int(input('Digite um número: '))
+for g in range(0, g + 1):
+    print(g)
+
+# _______________ Exemplo dado na aula
+
+
+i = int(input('Inicio: '))
+f = int(input('Fim: '))
+p = int(input('Passo: '))
+
+for q in range(i, f + 1, p):
+    print(q)
+    print("Um texto qualquer que vai se repetir dento do laço!")
+print("Fim")
