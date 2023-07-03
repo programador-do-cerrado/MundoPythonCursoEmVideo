91a669f2c79b76f63362bcd058fe74319fca09de
programador-do-cerrado <programadordocerrado@gmail.com>
2023-07-03 19:47:39 -0300

    Criação do exercicio 44  - exercicio completo

---
+++ Aulas/aula013.py
@@ -0,0 +1,15 @@
+"""
+    Exemplos de código em python
+    para criar laços de repetições
+"""
+print("Oi")
+print("Oi")
+print("Oi")
+print("Oi")
+print("Oi")
+print("Oi")
+
+# em vez disso -----------------------
+
+for c in range(1, 6):
+    print("Oi! dentro do laço ")
