26ce77a292f341ee240889e07816b6baaade4b87
programador-do-cerrado <programadordocerrado@gmail.com>
2023-07-04 20:28:39 -0300

    Exercico aula 43

--- Exercicios/ex049.py
+++ Exercicios/ex049.py
@@ -3,3 +3,6 @@ Refaça o DESAFIO 9, mostrando a tabuada de um número que o usuário escolher,
 só que agora utilizando um laço for.
 """
 
+num = int(input("Digite um numero inteiro para ver sua tabuada: "))
+for c in range(1, 11):
+    print('{} X {:2} = {} '.format(num, c, num * c))
