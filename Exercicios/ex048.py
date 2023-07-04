623a47751cd720c0328f1307b1d77166bff1120d
programador-do-cerrado <programadordocerrado@gmail.com>
2023-07-03 21:44:03 -0300

    Exercico aula 43

--- Exercicios/ex048.py
+++ Exercicios/ex048.py
@@ -3,3 +3,9 @@ Faça um programa que calcule a soma entre todos os números que são múltiplos
 e que se encontram no intervalo de 1 até 500.
 
 """
+soma = 0
+for i in range(1, 501, 2):
+    if i % 3 == 0:
+        #print(i, end=' ')
+        soma = soma + i
+print("A soma de todos os valores solicitados é {}".format(soma))
