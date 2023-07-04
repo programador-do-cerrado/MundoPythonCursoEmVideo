a1d9a885eaaa8acebb75577f79879107050cc5fb
programador-do-cerrado <programadordocerrado@gmail.com>
2023-07-03 21:14:28 -0300

    Exercico aula 43

--- Exercicios/ex047.py
+++ Exercicios/ex047.py
@@ -5,11 +5,11 @@ números pares que estão no intervalo entre 1 e 50.
 """
 print("Numeros pares que estão entre 1 e 50")
 for i in range(2, 51, 2):
-    print("{}".format(i))
+    print("{}: Minha versão em coluna".format(i))
 print("Acabou!!!")
 
 # Versão do professor
 
 for n in range(2, 51, 2):
     print(n, end=' ')
-print("Acabou!!!")
+print("Acabou!!! Versão do Professor em linha")
