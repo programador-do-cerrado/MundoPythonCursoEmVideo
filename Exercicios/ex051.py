d47f12b0f3d77b42efe063d12dbb525edd9a72df
programador-do-cerrado <programadordocerrado@gmail.com>
2023-07-04 21:31:29 -0300

    Exercico 50

---
+++ Exercicios/ex051.py
@@ -0,0 +1,12 @@
+"""
+Desenvolva um programa que leia o primeiro termo e a razão de uma PA.
+No final, mostre os 10 primeiros termos dessa progressão.
+
+"""
+a = float(input("Digite o primeiro termo da P.A.: "))
+r = float(input("Digite o valor da razão dessa P.A: (utilize . para separar as casas decimais)"))
+
+print("O primeiro termo dessa P.A. é {} e a razão é {}".format(a, r))
+
+for c in range(0, 11):
+   
