dc609bfc5dd77fefe2ef6cc0a39503923ff3f3b4
programador-do-cerrado <programadordocerrado@gmail.com>
2023-07-06 09:56:42 -0300

    Exercico 50

---
+++ Exercicios/ex052.py
@@ -0,0 +1,14 @@
+"""
+ Faça um programa que leia um número inteiro e diga se ele é ou não um número primo.
+
+"""
+
+num = int(input("Digite um numero: "))
+tot = 0
+# tot é o total de divisiveis
+for c in range(1, num + 1):
+    if num % c == 0:
+        print('\033[34m',end=' ' )
+    else:
+        print('\033[31m', end=' ')
+    print("{}".format(c), end=' ')
