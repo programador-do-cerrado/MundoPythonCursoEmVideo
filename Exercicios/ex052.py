a183ef3d41f85663631211d85d6cd358c6ab23b1
programador-do-cerrado <programadordocerrado@gmail.com>
2023-07-06 10:02:41 -0300

    Exercico 50

--- Exercicios/ex052.py
+++ Exercicios/ex052.py
@@ -8,8 +8,13 @@ tot = 0
 # tot é o total de divisiveis
 for c in range(1, num + 1):
     if num % c == 0:
-        print('\033[34m',end=' ' )
+        print('\033[34m', end=' ')
+        tot += 1
     else:
         print('\033[31m', end=' ')
     print("{}".format(c), end=' ')
-
+print("O número {} foi divisivel {} vezes".format(num, tot))
+if tot == 2:
+    print("O número {} não é primo".format(num))
+elif tot > 2:
+    print("O número {} não é primo.".format(num))
